b3f4c5e6d4c92ee401d04aeacf93bd62ef7b72d0
Wizered <64568264+Wizered@users.noreply.github.com>
2020-05-30 14:47:27 -0600

    Remove police car
    Removes the police car from the F4 Menu

--- lua/darkrp_customthings/vehicles.lua
+++ lua/darkrp_customthings/vehicles.lua
@@ -13,12 +13,3 @@ in the disabled_defaults.lua in the "darkrp_config" folder
 
 Add vehicles under the following line:
 ---------------------------------------------------------------------------]]
-
-DarkRP.createVehicle({
-	name = "charger12poltdm",
-	label = "Dodge Charger SRT8 2012 Police",
-	model = "models/tdmcars/emergency/dod_charger12.mdl",
-	angle = Angle(0, 0, 0),
-	price = 2000,
-	allowed = {TEAM_POLICE, TEAM_CHIEF}
-})
